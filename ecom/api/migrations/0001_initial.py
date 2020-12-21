07ddb9e0e0dbfdccd5cd63832ed63c3cf2d28d7a
Aradhi Sadrani <aradhisadrani@gmail.com>
2020-12-21 23:15:40 +0530

    Ecommerce Website Project

---
+++ ecom/api/migrations/0001_initial.py
@@ -0,0 +1,17 @@
+from django.db import migrations
+from api.user.models import CustomUser
+
+
+class Migration(migrations.Migration):
+    def seed_data(apps, schema_editor):
+        user = CustomUser(name="Aradhi", email="aradhisadrani@gmail.com",is_staff=True, is_superuser=True, phone="9427081381",gender="Female")
+        user.set_password("12345")
+        user.save()
+
+    dependencies = [
+        
+    ]
+
+    operations = [
+        migrations.RunPython(seed_data),
+    ]
